571ece12707de2a6faa41fecf991908cf54b3eaa
matheus abrantes <abranteselcio@gmail.com>
2024-12-24 12:33:19 -0300

    first commit

---
+++ src/main/java/com/fromhelstudio/fromhel_news/FromhelNewsApplication.java
@@ -0,0 +1,13 @@
+package com.fromhelstudio.fromhel_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromhelNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromhelNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fromhelstudio/fromhel_news/FromhelNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fromhelstudio.fromhel_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromhelNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
