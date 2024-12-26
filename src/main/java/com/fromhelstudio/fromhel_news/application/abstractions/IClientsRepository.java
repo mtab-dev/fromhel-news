463036591b493454744edb6cfa87f8e1f975c3aa
matheus abrantes <abranteselcio@gmail.com>
2024-12-26 20:28:05 -0300

    feat: new client method for repository

--- src/main/java/com/fromhelstudio/fromhel_news/application/abstractions/IClientsRepository.java
+++ src/main/java/com/fromhelstudio/fromhel_news/application/abstractions/IClientsRepository.java
@@ -1,7 +1,10 @@
 package com.fromhelstudio.fromhel_news.application.abstractions;
 
+import com.fromhelstudio.fromhel_news.domain.entities.ClientEntity;
+
 import java.util.Map;
 
 public interface IClientsRepository {
     Map<String, Object> getAllClients();
+    Map<String, Object> registerClient(ClientEntity client);
 }
