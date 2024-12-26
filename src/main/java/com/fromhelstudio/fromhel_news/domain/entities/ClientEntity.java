c81a3203c269c8ea119a2a7be2efdece27c7c291
matheus abrantes <abranteselcio@gmail.com>
2024-12-26 19:18:30 -0300

    refactor: user turned into client for semantic reasos

--- src/main/java/com/fromhelstudio/fromhel_news/domain/entities/ClientEntity.java
+++ src/main/java/com/fromhelstudio/fromhel_news/domain/entities/ClientEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserEntity {
+public class ClientEntity {
     private String clientId;
     private String clientName;
     private String email;
