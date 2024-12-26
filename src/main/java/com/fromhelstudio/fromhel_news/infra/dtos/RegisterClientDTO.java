d80839bc965e4348fb1f1873b54c21efc4de6838
matheus abrantes <abranteselcio@gmail.com>
2024-12-26 20:40:14 -0300

    feat: register client dto without lombok

--- src/main/java/com/fromhelstudio/fromhel_news/infra/dtos/RegisterClientDTO.java
+++ src/main/java/com/fromhelstudio/fromhel_news/infra/dtos/RegisterClientDTO.java
@@ -3,15 +3,23 @@ package com.fromhelstudio.fromhel_news.infra.dtos;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RegisterClientDTO {
     @NotNull
     private String clientName;
 
     @NotNull
     private String email;
+
+    public String getClientName(){
+        return clientName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+
 }
